1495180e13a389578ce81f585856e48312c71883
rara-dev <kksr1219@gmail.com>
2022-12-03 11:14:14 +0900

    사전 view 작업

--- src/main/java/com/rara/my_selectshop/controller/ShopController.java
+++ src/main/java/com/rara/my_selectshop/controller/ShopController.java
@@ -1,2 +1,16 @@
-package com.rara.my_selectshop.controller;public class ShopController {
+package com.rara.my_selectshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/api")
+public class ShopController {
+
+	@GetMapping("/shop")
+	public ModelAndView shop() {
+		return new ModelAndView("index");
+	}
 }
