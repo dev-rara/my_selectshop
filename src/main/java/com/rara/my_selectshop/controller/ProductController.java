0e2d018a8e388e836cfd02be0daf7e64aaf90a52
rara-dev <kksr1219@gmail.com>
2022-12-17 10:41:54 +0900

    AOP 적용

--- src/main/java/com/rara/my_selectshop/controller/ProductController.java
+++ src/main/java/com/rara/my_selectshop/controller/ProductController.java
@@ -34,35 +34,10 @@ public class ProductController {
 	// 관심 상품 등록하기
 //	@Secured(UserRoleEnum.Authority.ADMIN)
 	@PostMapping("/products")
-	public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-		//측정 시작 시간
-		Long startTime =System.currentTimeMillis();
-		try {
-			return productService.createProduct(requestDto, userDetails.getUser());
-		} finally {
-			//측정 종료 시간
-			Long endTime = System.currentTimeMillis();
+	public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto,
+		@AuthenticationPrincipal UserDetailsImpl userDetails) {
 
-			//수행시간 = 종료시간 - 시작시간
-			Long runTime = endTime - startTime;
-
-			//로그인 회원 정보
-			User loginUser = userDetails.getUser();
-
-			// API 사용시간 및 DB 에 기록
-			ApiUseTime apiUseTime = apiUseTimeRepository.findByUser(loginUser)
-				.orElse(null);
-			if (apiUseTime == null) {
-				// 로그인 회원의 기록이 없으면
-				apiUseTime = new ApiUseTime(loginUser, runTime);
-			} else {
-				// 로그인 회원의 기록이 이미 있으면
-				apiUseTime.addUseTime(runTime);
-			}
-
-			log.info("[API Use Time] Username: " + loginUser.getUsername() + ", Total Time: " + apiUseTime.getTotalTime() + " ms");
-			apiUseTimeRepository.save(apiUseTime);
-		}
+		return productService.createProduct(requestDto, userDetails.getUser());
 	}
 
 	// 관심 상품 조회하기
@@ -75,12 +50,14 @@ public class ProductController {
 		@AuthenticationPrincipal UserDetailsImpl userDetails
 	) {
 		// 응답 보내기
-		return productService.getProducts(userDetails.getUser(), page-1, size, sortBy, isAsc);
+		return productService.getProducts(userDetails.getUser(), page - 1, size, sortBy, isAsc);
 	}
 
 	// 관심 상품 최저가 등록하기
 	@PutMapping("/products/{id}")
-	public Long updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+	public Long updateProduct(@PathVariable Long id,
+		@RequestBody ProductMypriceRequestDto requestDto,
+		@AuthenticationPrincipal UserDetailsImpl userDetails) {
 		// 응답 보내기 (업데이트된 상품 id)
 		return productService.updateProduct(id, requestDto, userDetails.getUser());
 	}
